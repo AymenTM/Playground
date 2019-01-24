1eab433f3c8142b06b0a3f480e4bf05c7bf61253
AymenTM <idev.aymen@gmail.com>
2019-01-24 10:10:19 -0800

    Learning New Stuff

---
+++ Python/Flask/Blog/flaskr/blog.py
@@ -0,0 +1,36 @@
+
+# Blog Post Website
+# Credits: Youtube - Corey Schafer - Python Flask Tutorial
+
+from flask import Flask, render_template, url_for
+app = Flask(__name__)
+
+database = [
+    {
+        'author': 'John Doe',
+        'title': 'Blog Post 1',
+        'content': 'First Post Content',
+        'date_posted': 'April 20, 2018'
+    },
+    {
+        'author': 'Sam Carter',
+        'title': 'Blog Post 2',
+        'content': 'Second Post Content',
+        'date_posted': 'April 21, 2018'
+    },
+]
+
+
+@app.route("/")
+@app.route("/home")
+def home():
+    return render_template('home.html', posts=database)
+
+
+@app.route("/about")
+def about():
+    return render_template('about.html', title='About')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
