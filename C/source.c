d827defc159596ed7cae461495fcb613378a3c31
Lìon <idev.aymen@gmail.com>
2019-03-06 22:15:12 -0800

    Add files via upload

---
+++ C/source.c
@@ -0,0 +1,31 @@
+/*
+ *
+ * TEST MAIN
+ *
+ */
+
+#include ""
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
+typedef struct
+{
+	int bit1 : 1;
+	int bit2 : 1;
+	int bit3 : 1;
+	int bit4 : 1;
+	int bit5 : 1;
+	int bit6 : 1;
+	int bit7 : 1;
+	int bit8 : 1;
+};
+
+int	main(void)
+{
+
+
+	return (0);
+}
